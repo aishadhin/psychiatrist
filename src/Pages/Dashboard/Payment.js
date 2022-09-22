f1ed5872fafb58308e9610a491acec6e4eb8ff0a
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-22 00:06:42 -0500

    payment

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,6 +1,11 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51LkgWlHeuushmAdprVDAfnhsPKcsJbdJS5iQ1oEz1pJnnvD5CoUYYqDEHRlnWYYYULlnIQg7quTMPdKYY7dxDIDq00SGjkkd5H');
 
 const Payment = () => {
     const { id } = useParams();
@@ -21,22 +26,19 @@ const Payment = () => {
     return (
         <div>
             <h2>Please pay for {id}</h2>
-            <div className="hero bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="bg-base-200">
+                <div className="p-5">
                     <div className="card w-50 max-w-md bg-base-100 shadow-xl">
                         <div className="card-body">
                             <h2 className="card-title">Pay for {appointment.treatment}</h2>
                             <p>Hello {appointment.patientName}</p>
                             <p>Your appointment on {appointment.date} at {appointment.slot}</p>
                             <p>Amount ${appointment.price}</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Pay Now</button>
+                            <div className="card-body">
+                                <Elements stripe={stripePromise}>
+                                    <CheckoutForm appointment={appointment}/>
+                                </Elements>
                             </div>
-                        </div>
-                    </div>
-                    <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                        <div className="card-body">
-
                         </div>
                     </div>
                 </div>
